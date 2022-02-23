b0ba7e0b057de5861e8494715e579252ac3d4619
larobitrumpet <larobitrumpet@lukerobillard.xyz>
2022-02-22 16:00:53 -0800

    Add NODE struct to store information for each step in the flow chart

--- map_age_guide.c
+++ map_age_guide.c
@@ -1,5 +1,19 @@
 #include <curses.h>
 
+struct NODE
+{
+    char* message;
+    char* choice1m;
+    char* choice2m;
+    char* choice3m;
+    char* choice4m;
+    struct NODE* choice1;
+    struct NODE* choice2;
+    struct NODE* choice3;
+    struct NODE* choice4;
+    struct NODE* prev;
+} NODE;
+
 int main()
 {
     return 0;
